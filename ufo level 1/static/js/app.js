b413a5936833c452e205829228ae5d936cfd6b31
soyeonjk <80230842+soyeonjk@users.noreply.github.com>
2021-06-22 19:56:27 -0400

    Add files via upload

---
+++ ufo level 1/static/js/app.js
@@ -0,0 +1,54 @@
+// Starter Code
+var tableData = data;
+
+
+// creating references 
+var $tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputFieldDate = d3.select("#datetime");
+var inputFieldCity = d3.select("#city");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+
+// inputing the data into the HTML
+var addData = (dataInput) => {
+    dataInput.forEach(ufoSightings => {
+        var row = $tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufoSightings[column])
+        )
+    });
+}
+
+addData(tableData);
+
+
+// creating an event listener to button
+// setting up the filter button for date and city
+button.on("click", () => {
+
+    d3.event.preventDefault();
+    
+
+    var inputDate = inputFieldDate.property("value").trim();
+    // console.log(inputDate)
+    // trim the inputs 
+    var filterDate = tableData.filter(tableData => tableData.datetime === inputDate);
+    
+
+    $tbody.html("");
+
+    let response = {
+        filterDate
+    }
+
+
+    if(response.filterDate.length !== 0) {
+        addData(filterDate);
+    }
+
+    // add comment if not sightings
+    
+        else {
+            $tbody.append("tr").append("td").text("No Sightings Here...Move On...");
+        }
+})
